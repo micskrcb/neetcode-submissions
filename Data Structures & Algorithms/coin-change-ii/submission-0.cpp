d84bf38daa2696533b6cade4f84d8c50107a8beb
micskrcb <157268006+micskrcb@users.noreply.github.com>
2026-05-27 13:25:39 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
+        
+            for(auto a:coins){
+                for(int i =0;i<=amount;i++){
+                    if(i-a>=0){
+                        dp[i]+=dp[i-a];
+                    }
+                }
+
+            }
+            return dp[amount];
+               
+    }
+};
